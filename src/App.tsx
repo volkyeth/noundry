f9607a3316c024db543cc7cb2db870e77905bc2f
Bruno Wowk <bruno.wowk@gmail.com>
2022-12-07 08:57:00 -0300

    Add ColorModeFixer to rectify light color mode on local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, useColorMode } from "@chakra-ui/react";
 import "@fontsource/press-start-2p/400.css";
 import { Route, Routes } from "react-router-dom";
 import { Editor } from "./components/pages/Editor";
@@ -6,8 +6,9 @@ import { PaletteFixer } from "./components/pages/PaletteFixer";
 import theme from "./theme";
 import { useWebVitals } from "./webVitals";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { useEffect } from "react";
 
-function App() {
+const App = () => {
   if (import.meta.env.PROD) {
     useWebVitals();
   }
@@ -16,6 +17,7 @@ function App() {
 
   return (
     <ChakraProvider theme={theme} resetCSS>
+      <ColorModeFixer />
       <QueryClientProvider client={queryClient}>
         <Routes>
           <Route path="/" element={<Editor />} />
@@ -24,6 +26,18 @@ function App() {
       </QueryClientProvider>
     </ChakraProvider>
   );
-}
+};
+
+// @TODO Remove this. It's here to fix the color mode on local storage for people that opened the app before the color mode was (correctly) set to dark.
+const ColorModeFixer = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  useEffect(() => {
+    if (colorMode === "light") {
+      toggleColorMode();
+    }
+  }, [colorMode]);
+
+  return <></>;
+};
 
 export default App;
