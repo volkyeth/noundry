25ed19cc84b915238d8fa1557d9a20830fd5b7b2
Volky <volkyeth@pm.me>
2023-09-27 10:20:57 -0300

    Disable react strict mode to fix modal issue

--- apps/studio/src/main.tsx
+++ apps/studio/src/main.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
