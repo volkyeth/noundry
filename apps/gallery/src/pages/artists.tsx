c2bec041027d228b64847acb46a795f769235a25
Volky <volkyeth@pm.me>
2023-11-17 16:10:37 -0300

    show trait count only

--- apps/gallery/src/pages/artists.tsx
+++ apps/gallery/src/pages/artists.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { getArtistStats } from "@/app/api/artists/stats/getArtistStats";
-import { TraitIcon } from "@/components/TraitIcon";
 import { UserBadge } from "@/components/UserBadge";
 import { UserStats } from "@/types/user";
 import { useQuery } from "@tanstack/react-query";
@@ -32,8 +31,8 @@ const ArtistsPage: NextPage<{ artistsStats: UserStats[] }> = ({
 
   return (
     <div className="container mx-auto py-10 flex flex-col items-center  gap-6  lg:px-10 md:px-5 sm:px-6  px-6">
-      {/*<h1>Artists</h1>*/}
-      <div className="grid w-fit grid-cols-[1fr_max-content] xs:grid-cols-[repeat(4,max-content)] mt-10 gap-x-6 gap-y-2 text-default">
+      <h1>Artists</h1>
+      <div className="grid w-fit grid-cols-[1fr_max-content] mt-10 gap-x-6 gap-y-2 text-default">
         {artistsStats?.map((artist) => (
           <>
             <Link
@@ -48,20 +47,6 @@ const ArtistsPage: NextPage<{ artistsStats: UserStats[] }> = ({
               className="flex gap-2 justify-end items-center "
             >
               <p>{artist.traits} traits</p>
-            </div>
-            <div
-              key={`head-count-${artist.address}`}
-              className="gap-2 items-center justify-end hidden xs:flex"
-            >
-              <p>{artist.heads}</p>
-              <TraitIcon className="w-6" type="heads" />
-            </div>
-            <div
-              key={`accessory-count-${artist.address}`}
-              className="gap-2 items-center justify-end hidden xs:flex"
-            >
-              <p>{artist.accessories}</p>
-              <TraitIcon className="w-6" type="accessories" />
             </div>
           </>
         ))}
