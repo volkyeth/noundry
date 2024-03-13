20ea4242023ddf1def8f44e5ac14b8991cfa0381
Volky <volkyeth@pm.me>
2024-03-13 16:20:15 -0300

    Enable twitter bot

--- apps/yellow-gallery/src/app/api/inngest/route.ts
+++ apps/yellow-gallery/src/app/api/inngest/route.ts
@@ -3,9 +3,10 @@ import { serve } from "inngest/next";
 import { dumpTraits } from "./handlers/db/dump/dumpTraits";
 import { dumpUsers } from "./handlers/db/dump/dumpUsers";
 import { runScript } from "./handlers/runScript";
+import { castOnFarcaster } from "./handlers/trait/submitted/castOnFarcaster";
+import { postOnTwitter } from "./handlers/trait/submitted/postOnTwitter";
 import { upsertUser } from "./handlers/user/signedIn/upsertUser";
 import { updateCordUser } from "./handlers/user/updated/updateCordUser";
-import { castOnFarcaster } from "./handlers/trait/submitted/castOnFarcaster";
 
 export const maxDuration = 300;
 
@@ -14,7 +15,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     castOnFarcaster,
     // postOnDiscord,
-    // postOnTwitter,
+    postOnTwitter,
     dumpTraits,
     dumpUsers,
     runScript,
