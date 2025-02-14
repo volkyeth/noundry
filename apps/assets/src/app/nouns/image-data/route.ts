1118b56ca76bd089a3f5f4fc3e9591609b6e9b69
Volky <volkyeth@pm.me>
2025-02-14 14:55:24 -0300

    do not prerender image-data router

--- apps/assets/src/app/nouns/image-data/route.ts
+++ apps/assets/src/app/nouns/image-data/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import { fetchNounsArtwork } from "noggles";
 
 export const revalidate = 900;
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
   const response = NextResponse.json(await fetchNounsArtwork(publicClient));
