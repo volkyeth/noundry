356176e0e2c7f83c014662c7fe8995ef7f178ebf
Volky <volkyeth@pm.me>
2025-02-17 16:27:38 -0300

    prettify json

--- apps/assets/src/app/nouns/image-data.json/route.ts
+++ apps/assets/src/app/nouns/image-data.json/route.ts
@@ -5,7 +5,11 @@ import { fetchOnchainNounsArtData, nounsTraitNames, toImageData } from "noggles"
 export const revalidate = 900;
 
 export async function GET() {
-  const response = NextResponse.json(toImageData(await fetchOnchainNounsArtData(publicClient), nounsTraitNames));
+  const response = new NextResponse(JSON.stringify(toImageData(await fetchOnchainNounsArtData(publicClient), nounsTraitNames), null, 2), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
   response.headers.set('Cache-Control', `public, s-maxage=${revalidate}, stale-while-revalidate=604800`);
 
