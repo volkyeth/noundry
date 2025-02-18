240600b6850ea19657f873f273d0807f922bc2c7
Volky <volkyeth@pm.me>
2025-02-18 10:54:06 -0300

    fetch artwork from assets.noundry.wtf

--- apps/gallery/src/app/api/nouns/traits/route.ts
+++
@@ -1,8 +0,0 @@
-import { publicClient } from "@/app/publicClient";
-import { NextResponse } from "next/server";
-import { fetchNounsArtwork } from "noggles";
-
-export async function GET() {
-  // TODO cache for 15min
-  return NextResponse.json(await fetchNounsArtwork(publicClient));
-}
--- apps/gallery/src/hooks/useMainnetArtwork.ts
+++ apps/gallery/src/hooks/useMainnetArtwork.ts
@@ -4,6 +4,6 @@ import { OnchainArtwork } from "noggles";
 export const useMainnetArtwork = () =>
   useQuery<OnchainArtwork>({
     queryKey: ["mainnet-artwork"],
-    queryFn: () => fetch("/api/nouns/traits").then((r) => r.json()),
+    queryFn: () => fetch("https://assets.noundry.wtf/nouns/art-data.json").then((r) => r.json()),
     staleTime: 1000 * 60 * 15,
   });
