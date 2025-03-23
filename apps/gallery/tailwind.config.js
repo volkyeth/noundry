f7d16e56ec96eb94b95c342c8d14cea730bce4ca
Volky <volkyeth@pm.me>
2025-03-23 19:18:38 -0300

    add variants folder to tailwind config

--- apps/gallery/tailwind.config.js
+++ apps/gallery/tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
   darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/variants/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
