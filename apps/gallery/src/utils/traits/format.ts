090105d2048e097e4a3266eefd4665a47dfb43eb
Volky <volkyeth@pm.me>
2025-05-31 19:24:12 -0300

    chore(gallery): add formatSubmissionType

--- apps/gallery/src/utils/traits/format.ts
+++ apps/gallery/src/utils/traits/format.ts
@@ -1,4 +1,6 @@
 import { TraitCategory, TraitType } from "noggles";
+import { SubmissionType, SubmissionCategory } from "@/types/submission";
+import { appConfig } from "@/variants/config";
 
 export const formatTraitType = (type?: TraitCategory | TraitType | null) => {
   switch (type) {
@@ -16,6 +18,30 @@ export const formatTraitType = (type?: TraitCategory | TraitType | null) => {
     case "backgrounds":
     case "background":
       return "background";
+    default:
+      return "";
+  }
+};
+
+export const formatSubmissionType = (type?: SubmissionCategory | SubmissionType | null) => {
+  switch (type) {
+    case "heads":
+    case "head":
+      return "head";
+    case "glasses":
+      return "noggles";
+    case "accessories":
+    case "accessory":
+      return "accessory";
+    case "bodies":
+    case "body":
+      return "body";
+    case "backgrounds":
+    case "background":
+      return "background";
+    case "nouns":
+    case "noun":
+      return appConfig.nounTerm;
     default:
       return "";
   }
