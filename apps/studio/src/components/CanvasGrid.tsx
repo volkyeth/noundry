1aa919cfd77f7573453c4d255e33117125809477
Volky <volkyeth@pm.me>
2025-06-17 15:17:36 -0300

    fix(studio): fix canvas grid

--- apps/studio/src/components/CanvasGrid.tsx
+++ apps/studio/src/components/CanvasGrid.tsx
@@ -11,8 +11,7 @@ export const CanvasGrid: FC<CanvasGridProps> = ({ children, cells = 32, ...props
       borderRightWidth={1}
       borderBottomWidth={1}
       bgSize={`${100 / cells}% ${100 / cells}%`}
-      bgGradient="repeating-linear-gradient(#ccc 0 1px, transparent 1px 100%),
-      repeating-linear-gradient(90deg, #ccc 0 1px, transparent 1px 100%)"
+      bgGradient="repeating-linear-gradient(#ccc 0 1px, transparent 1px 100%), repeating-linear-gradient(90deg, #ccc 0 1px, transparent 1px 100%)"
       {...props}
     >
       {children}
