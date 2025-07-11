08066f8d41ea91a7bb6e56845409d44b35d1651b
Volky <volkyeth@pm.me>
2025-07-11 13:51:16 -0300

    feat(lil-studio): enable submitting to lil gallery

--- apps/studio/src/variants/lil-nouns/config.ts
+++ apps/studio/src/variants/lil-nouns/config.ts
@@ -4,6 +4,7 @@ import Logo from "./assets/lil-studio.svg?react";
 
 export const lilNounsConfig: AppConfig = {
     variant: 'lil-nouns' as AppVariant,
+    galleryUrl: import.meta.env.VITE_GALLERY_URL || 'https://lil.noundry.wtf',
     imageDataScriptUri: 'https://assets.noundry.wtf/lil-nouns/image-data.js',
     appTitle: 'Lil Studio',
     Logo,
